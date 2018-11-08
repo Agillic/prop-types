78de9b8193cd77da0e611a1bd5fd0a7eb5725052
Krzysztof Czurylo <krzysztof.czurylo@agillic.com>
2018-11-08 12:37:39 +0000

    Changed notOnlyWhiteSpace prop-type to uncurried version

--- src/notOnlyWhiteSpace.js
+++ src/notOnlyWhiteSpace.js
@@ -1,4 +1,4 @@
-const notOnlyWhiteSpace = () => ((props, propName, componentName) => {
+const notOnlyWhiteSpace = ((props, propName, componentName) => {
   if ((typeof props[propName] === 'string') && (props[propName].trim().length < 1)) {
     return new TypeError(`${propName} should not contain only whitespace in ${componentName}`)
   }
