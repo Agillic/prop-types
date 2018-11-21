fc8ce59299030fefd7a8cf1c491bde1646d6a5bb
Krzysztof Czurylo <kabece@gmail.com>
2018-11-21 13:09:04 +0100

    Implemented noOp thunks

---
+++ src/validators.js
@@ -0,0 +1,26 @@
+import differentFrom from './differentFrom'
+import forbiddenBy from './forbiddenBy'
+import greaterThan from './greaterThan'
+import minimumLength from './minimumLength'
+import notOnlyWhiteSpace from './notOnlyWhiteSpace'
+import requiredBy from './requiredBy'
+
+const propTypesValidators = {
+  differentFrom,
+  forbiddenBy,
+  greaterThan,
+  minimumLength,
+  notOnlyWhiteSpace,
+  requiredBy
+}
+
+export {
+  differentFrom,
+  forbiddenBy,
+  greaterThan,
+  minimumLength,
+  notOnlyWhiteSpace,
+  requiredBy
+}
+
+export default propTypesValidators
